7eb23ce65ad9e8a2f202fdf4ca89193aeb073519
Supun Wimukthi <supunwimukthi@gmail.com>
2017-12-22 22:01:38 +0530

    Implementation relevant to Encapsulation

--- src/com/app/oop/encapsulation/EncapsulationExample.java
+++ src/com/app/oop/encapsulation/EncapsulationExample.java
@@ -2,4 +2,13 @@ package com.app.oop.encapsulation;
 
 public class EncapsulationExample {
 
+	public static void main(String[] args) {
+		
+		Person person = new Person();
+		person.setName("John Doe");
+		person.setAge(25);
+		person.setEmail("johndoe@gmail.com");
+		person.setTelephone(001125343555);
+	}
+	
 }
---
+++ src/com/app/oop/encapsulation/Person.java
@@ -0,0 +1,36 @@
+package com.app.oop.encapsulation;
+
+public class Person {
+
+	private String name;
+	private int age;
+	private int telephone;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
